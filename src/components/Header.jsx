d08981ea437662959034cc9a707966ae2e99a8c4
Asif Ameen <asifmameen@gmail.com>
2025-04-06 19:02:56 +0530

    implementing header home page navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,15 @@
-import { Search } from "@mui/icons-material";
-import {
-  AppBar,
-  Avatar,
-  Box,
-  Button,
-  CardMedia,
-  IconButton,
-  InputBase,
-  Menu,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Button, CardMedia, Toolbar } from "@mui/material";
 import mainLogo from "../assets/images/evergreen_logo.png";
 import { useGlobalState } from "../context/GlobalStateContext";
 import ContactModal from "../pages/ContactModal";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { openContactModal, setOpenContactModal } = useGlobalState();
+  const navigate = useNavigate();
+  const homePageNav = () => {
+    navigate("/");
+  };
   return (
     <>
       <AppBar
@@ -30,18 +23,20 @@ const Header = () => {
             alignItems: "center",
           }}
         >
-          <CardMedia
-            sx={{
-              width: 1030,
-              height: 60,
-              borderRadius: 3,
-              backgroundSize: "contain",
-              objectFit: "contain",
-              backgroundImage: `url(${mainLogo})`,
-              marginLeft: "-307px",
-            }}
-            alt="Spices"
-          />
+          <Button onClick={homePageNav}>
+            <CardMedia
+              sx={{
+                width: 1030,
+                height: 60,
+                borderRadius: 3,
+                backgroundSize: "contain",
+                objectFit: "contain",
+                backgroundImage: `url(${mainLogo})`,
+                marginLeft: "-307px",
+              }}
+              alt="Spices"
+            />
+          </Button>
 
           {/* Right: Buttons and Avatar */}
           <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
